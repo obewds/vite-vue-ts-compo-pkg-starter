8225aa8eac25ff0fa04fcaa57c14fd9078153de8
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-04-25 03:18:34 -0400

    cleaned up workspace app design & added ts logo

--- src/app/HelpContent.vue
+++ src/app/HelpContent.vue
@@ -11,13 +11,13 @@
     // setup static values
     const more = 'more things'
     const less = 'less things'
-    const toDosWip = 'STARTING THE THINGS'
+    const toDosWip = 'START THE THINGS'
     const toDosDone = 'DID THE THINGS'
     const btnClasses = 'inline text-white rounded px-2 py-1 shadow'
     const btnStyle = 'background-color:#42b983;'
     const flexColSect = 'flex flex-col space-y-2'
     const h2Classes = 'text-lg font-bold'
-    const imgClasses = 'w-24 mx-auto sm:mx-0'
+    const imgClasses = 'w-20 mx-auto sm:mx-0'
     const inputClasses = 'focus:ring-green-500 focus:border-green-500 block w-full shadow-sm border-gray-300 rounded-md'
     const readonlyInputClasses = 'focus:ring-lime-500 focus:border-lime-500 block w-full shadow-sm border-black bg-gray-800 text-lime-400 rounded-md selection:bg-lime-400 selection:text-black'
     const linkStyle = 'color:#42b983;'
@@ -37,17 +37,17 @@
     const newRepoName = ref(storedRepoName)
     const showDocs = ref(false)
     const showDocsBtnText = ref(more)
-    const showToDos = ref(true)
-    const showToDosBtnText = ref(less)
+    const showToDos = ref(false)
+    const showToDosBtnText = ref(more)
     const toDosHeadline = ref(toDosHeadlineText)
 
 
     // adjust states according if local storage data is complete on page load
-    if (localStorage.length === 24) {
-        toDosHeadline.value = toDosDone
-        showToDos.value = false
-        showToDosBtnText.value = more
-    }
+    // if (localStorage.length === 24) {
+    //     toDosHeadline.value = toDosDone
+    //     showToDos.value = false
+    //     showToDosBtnText.value = more
+    // }
 
     
     // setup computed values
@@ -74,9 +74,9 @@
         } else {
             localStorage.setItem('repoName', newRepoName.value)
         }
-        if (localStorage.length === 24) {
-            toDosHeadline.value = toDosDone
-        }
+        // if (localStorage.length === 24) {
+        //     toDosHeadline.value = toDosDone
+        // }
     }
 
     function reset(){
@@ -93,11 +93,13 @@
 <template>
 
     <div class="w-full flex flex-col sm:flex-row justify-center text-center gap-8 pb-10 px-8">
-        <img :class="imgClasses" alt="Vue logo" src="../assets/logo.png"/>
+        <img :class="imgClasses" alt="Vue logo" src="../assets/vue-logo.png"/>
+        <img :class="imgClasses" alt="TypeScript logo" src="../assets/typescript-logo.png"/>
         <img :class="imgClasses" alt="OBE:WDS logo" src="../assets/OBE-MARK-200x200.png"/>
     </div>
 
-    <h1 class="text-3xl pb-14">OBE:WDS Vite/Vue Compo Starter</h1>
+    <h1 class="text-3xl pb-2">OBE:WDS Vite/Vue TypeScript</h1>
+    <p class="text-xl pb-14">Component Package Dev Kit</p>
 
     <slot/>
 
@@ -156,7 +158,7 @@
 
     <div :class="[flexColSect, 'pt-8 pb-20']">
 
-        <h2 :class="h2Classes">READING THE THINGS</h2>
+        <h2 :class="h2Classes">READ THE THINGS</h2>
 
         <p class="pb-2">
             <button
@@ -185,14 +187,24 @@
                     <a href="https://vitest.dev/" target="_blank" :style="linkStyle" rel="noopener noreferrer">Vitest Docs</a>
                 </li>
                 <li>
+                    <a href="https://test-utils.vuejs.org/guide/" target="_blank" :style="linkStyle" rel="noopener noreferrer">Vue Test Utils</a>
+                </li>
+                <li>
                     <a href="https://tailwindcss.com/docs/" target="_blank" :style="linkStyle" rel="noopener noreferrer">Tailwind CSS Docs</a>
+                </li>
+                <li>
+                    <a href="https://headlessui.dev/" target="_blank" :style="linkStyle" rel="noopener noreferrer">Headless UI</a>
                 </li>
                 <li>
                     <a href="https://rollupjs.org/guide/en/" target="_blank" :style="linkStyle" rel="noopener noreferrer">Rollup Docs</a>
                 </li>
             </ul>
 
-            <p>Recommended IDE setup: <a href="https://code.visualstudio.com/" target="_blank" :style="linkStyle" rel="noopener noreferrer">VS Code</a> + <a href="https://github.com/johnsoncodehk/volar" target="_blank" :style="linkStyle" rel="noopener noreferrer">Volar</a></p>
+            <p class="pt-2">
+                Recommended IDE setup:
+                <br>
+                <a href="https://code.visualstudio.com/" target="_blank" :style="linkStyle" rel="noopener noreferrer">VS Code</a> + <a href="https://github.com/johnsoncodehk/volar" target="_blank" :style="linkStyle" rel="noopener noreferrer">Volar</a>
+            </p>
 
         </div>
     
